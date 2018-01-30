2473a8685b8201870bac323a4feface24c72908f
Maximilian Guttmann <maximilian.guttmann@bnet.at>
2018-01-30 15:02:08 +0100

    Variablen hinzugefügt und setter + getter methoden ausprogrammiert

--- Dominion/src/daten/Karte.java
+++ Dominion/src/daten/Karte.java
@@ -1,5 +1,50 @@
 package daten;
 
-public class Karte {
+public abstract class Karte {
 
+	private int m_kosten;
+	private String m_name;
+	private String m_nutzen;
+	private String m_typ;
+	
+	Karte(){
+		
+	}
+	
+	private void kosten_set(int kosten) {
+		m_kosten = kosten;
+	}
+	
+	private void name_set(String name) {
+		m_name = name;
+	}
+	
+	private void nutzen_set(String nutzen) {
+		m_nutzen = nutzen;
+	}
+	
+	private void typ_set(String typ) {
+		m_typ = typ;
+	}
+	
+	private int kosten_get() {
+		return m_kosten;
+	}
+	
+	private String name_get() {
+		return m_name;
+	}
+	
+	private String nutzen_get() {
+		return m_nutzen;
+	}
+	
+	private String typ_get() {
+		return m_typ;
+	}
+	
+	private void init(int kosten) {
+		kosten_set(kosten);
+	}
+	
 }
